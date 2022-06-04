04c85995a2cb61dfff5f65d8cc6ab2fc5342f571
Harri Häivälä <haivala@cc.hut.fi>
2022-06-04 11:27:58 +0300

    update script

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+dc=$(which docker-compose)
+
+touch ./Lychee/.NO_AUTO_COMPOSER_MIGRATE;
+$dc build --pull;
+$dc up -d;
+cd Lychee;
+git pull;
+cd ..;
+$dc exec fpm /usr/bin/composer.phar install -o --no-dev --prefer-dist;
+$dc exec fpm ./artisan migrate --force
